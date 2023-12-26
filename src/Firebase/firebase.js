d1f5c9661a1e5d1adc7b874c2aedefd1fc25796a
kartik sharma <kartik200327@gmail.com>
2023-12-26 13:44:57 +0530

    frontend completed, firebase account has been setup

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBC-naRRpvqHSK_dSgJavaeqgDYf867FnA",
+  authDomain: "social-media-insta-33f27.firebaseapp.com",
+  projectId: "social-media-insta-33f27",
+  storageBucket: "social-media-insta-33f27.appspot.com",
+  messagingSenderId: "822849758116",
+  appId: "1:822849758116:web:5ffbb943aa65cbd9995731",
+  measurementId: "G-7REPE25QDQ"
+};
+
+
+const app = initializeApp(firebaseConfig);
